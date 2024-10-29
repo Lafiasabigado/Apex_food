ceff866bf57ffd53ed8611ab7728a235f65efa0b
Lafiasabigado <lafiasabigado97@gmail.com>
2024-10-29 14:18:09 +0100

    clerk config 2

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,7 +3,7 @@ import { SignIn } from 'vue-clerk'
 </script>
 
 <template>
-    <div class="justify-center items-center">
+    <div class="justify-center items-center mt-20 flex">
         <SignIn path="/sign-in" />
     </div>
 </template>
