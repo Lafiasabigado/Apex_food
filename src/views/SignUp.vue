5b9c471b18d6e36a8a45b2646fac6ba2d4912d52
Lafiasabigado <lafiasabigado97@gmail.com>
2024-10-30 13:12:15 +0100

    clerk config continue

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,7 +3,11 @@ import { SignUp } from 'vue-clerk'
 </script>
 
 <template>
-  <div class="justify-center flex mt-10">
-    <SignUp path="/sign-up" afterSignUpUrl="/" sign-in-url="/sign-in"/>
+  <div class="justify-center items-center mt-10 flex">
+    <SignUp 
+      path="/sign-up"
+      afterSignUpUrl="/" 
+      signInUrl="/sign-in"
+    />
   </div>
 </template>
