47d0daf0f1fead35f33ff7dc6c017efa63231da0
Lafiasabigado <lafiasabigado97@gmail.com>
2024-11-05 15:25:10 +0100

    clerk config

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,34 @@
 <script setup>
-import { SignIn } from 'vue-clerk'
+import { SignIn, useAuth } from 'vue-clerk'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const { isSignedIn } = useAuth()
+const router = useRouter()
+const signInMessage = ref('')
+
+onMounted(() => {
+  if (isSignedIn.value) {
+    signInMessage.value = 'Vous êtes déjà connecté.'
+    router.push('/')  // Redirection vers la page d'accueil si déjà connecté
+  }
+})
+
+// Mettre à jour le message lorsque la connexion réussit
+const handleSignIn = () => {
+  signInMessage.value = 'Connexion réussie !'
+  setTimeout(() => router.push('/'), 1000)  // Redirection après un court délai
+}
 </script>
 
 <template>
-  <div class="justify-center items-center mt-20 flex">
+  <div class="justify-center items-center mt-20 flex flex-col">
     <SignIn 
       path="/sign-in" 
       afterSignInUrl="/" 
       signUpUrl="/sign-up"
+      @signIn={handleSignIn}
     />
+    <p v-if="signInMessage" class="text-green-500 mt-4">{{ signInMessage }}</p>
   </div>
 </template>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,13 +1,34 @@
 <script setup>
-import { SignUp } from 'vue-clerk'
+import { SignUp, useAuth } from 'vue-clerk'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const { isSignedIn } = useAuth()
+const router = useRouter()
+const signUpMessage = ref('')
+
+onMounted(() => {
+  if (isSignedIn.value) {
+    signUpMessage.value = 'Vous êtes déjà inscrit et connecté.'
+    router.push('/')  // Redirection vers la page d'accueil si déjà connecté
+  }
+})
+
+// Mettre à jour le message lorsque l'inscription réussit
+const handleSignUp = () => {
+  signUpMessage.value = 'Inscription réussie !'
+  setTimeout(() => router.push('/'), 1000)  // Redirection après un court délai
+}
 </script>
 
 <template>
-  <div class="justify-center items-center mt-10 flex">
+  <div class="justify-center items-center mt-10 flex flex-col">
     <SignUp 
       path="/sign-up"
       afterSignUpUrl="/" 
       signInUrl="/sign-in"
+      @signUp={handleSignUp}
     />
+    <p v-if="signUpMessage" class="text-green-500 mt-4">{{ signUpMessage }}</p>
   </div>
 </template>
